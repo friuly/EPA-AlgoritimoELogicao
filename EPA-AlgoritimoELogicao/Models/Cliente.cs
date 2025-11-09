d8a759a78a70aaf64e43748ed4f55b9c89cbae38
andre <andre@NITRO5>
2025-11-09 17:32:17 -0300

    Adicionar arquivos de projeto.

---
+++ EPA-AlgoritimoELogicao/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace EPA_AlgoritimoELogicao.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; };
+        public string Telefonr { get; set; };
+        public string Email { get; set; };
+        public int Idade { get; set; };
+        public string CPF { get; set; };
+
+        public DateTime CarimboCadstro  { get; set; };
+    }
+}
---
+++ EPA-AlgoritimoELogicao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EPA_AlgoritimoELogicao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
